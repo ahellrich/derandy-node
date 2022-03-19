ddcaa8fdb1660e2fb0e465731ed19c92f748237c
Andreas Hellrich <andreas.hellrich@gmail.com>
2022-03-19 16:35:11 +0100

    fixed index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT;
+const path = require('path');
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/html/index.html'));
