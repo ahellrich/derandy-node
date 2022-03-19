8bf22ebd1a215e7b931d8baed770397af601b2e7
Andreas Hellrich <andreas.hellrich@gmail.com>
2022-03-19 21:00:40 +0100

    added php

--- public/scripts/js/alert.js
+++ public/scripts/js/alert.js
@@ -1,3 +1,16 @@
 function gib_laut(){
-    alert("wuff!");
+
+    $.ajax({
+            url:"test.php",    //the page containing php script
+            type: "post",    //request type,
+            dataType: 'json',
+            data: {x: 3, y: 5},
+            success:function(result){
+				
+                alert(result);
+            },
+            error:function(){
+				console.log("error");
+			}
+        });
 }
