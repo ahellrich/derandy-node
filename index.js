1551a740225e374ca9e6008564d649762419cc55
Andreas Hellrich <andreas.hellrich@gmail.com>
2022-03-19 15:56:07 +0100

    fixed index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express();
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(() => {
-  console.log(`Example app listening!`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
 });
