eb1563da9ad91637167fcdac2cd02cc7167d4041
Andreas Hellrich <andreas.hellrich@gmail.com>
2022-03-19 19:53:28 +0100

    used a require function found on stackoverflow to require js in fromtend

--- public/scripts/js/index-frontend.js
+++ public/scripts/js/index-frontend.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event){
-   var alert = $.getScript("alert.js");
+   var alert = require("alert.js");
    alert.gibLaut();
   });
+
+  function require(script) {
+    $.ajax({
+        url: script,
+        dataType: "script",
+        async: false,           // <-- This is the key
+        success: function () {
+            // all good...
+        },
+        error: function () {
+            throw new Error("Could not load script " + script);
+        }
+    });
+}
