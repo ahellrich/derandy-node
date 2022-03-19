401698ce56ef36e96e28e459c53328262d253ad4
Andreas Hellrich <andreas.hellrich@gmail.com>
2022-03-19 16:51:37 +0100

    made app ready to serve css files

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const port = process.env.PORT;
 const path = require('path');
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/html/index.html'));
+    res.sendFile(path.join(__dirname, 'public/html/index.html'));
 });
 
 app.listen(port);
