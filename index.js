04ac22ebbefff5b375c875be53b4ef954f93fa99
Andreas Hellrich <andreas.hellrich@gmail.com>
2022-03-19 17:33:58 +0100

    fixed import hopefully

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT;
 const path = require('path');
-const alert = require("./js-modules/alert.js").default;
+const alert = require("./js-modules/alert");
 
 alert.alert();
 
