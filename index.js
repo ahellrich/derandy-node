d22048a5c360384a71ebfe803446ac0fc7b15da8
Andreas Hellrich <andreas.hellrich@gmail.com>
2022-03-20 14:48:56 +0100

    using cors now

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/html/index.html'));
 });
