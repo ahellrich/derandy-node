13002cf45a81697eae20202f3541f32436806c08
Andreas Hellrich <andreas.hellrich@gmail.com>
2022-03-20 15:02:52 +0100

    made some updates

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT;
 const path = require('path');
-const cors = require('cors');
 
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors);
+express.use(cors());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/html/index.html'));
