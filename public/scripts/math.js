43156235c1fc5f6e06697d9b12a820aae1274381
Andreas Hellrich <andreas.hellrich@gmail.com>
2022-03-20 17:30:08 +0100

    final commit

--- public/scripts/math.js
+++ public/scripts/math.js
@@ -6,7 +6,6 @@ function math(op){
             dataType: 'json',
             data: {x: xval, y: yval},
             success:function(result){
-				alert(result);
                 showResult(result);
             },
             error:function(){
