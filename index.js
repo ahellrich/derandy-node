0ab0f9393cc4653699a45b2eadcf24c2e63d9e80
Andreas Hellrich <andreas.hellrich@gmail.com>
2022-03-20 14:53:13 +0100

    fixed index.js

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT;
 const path = require('path');
+const cors = requite('cors');
 
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
+app.use(cors);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/html/index.html'));
