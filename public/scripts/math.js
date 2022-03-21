2b791bbdedb87d79dce371ec7a833ce096e30410
Andreas Hellrich <andreas.hellrich@gmail.com>
2022-03-21 09:25:23 +0100

    updated source.html and removed comments form math.js

--- public/scripts/math.js
+++ public/scripts/math.js
@@ -1,10 +1,10 @@
 function math(op){
     setVariables();
     $.ajax({
-            url:"https://derandy-php.herokuapp.com/math.php",    //the page containing php script
-            type: "post",    //request type,
+            url:"https://derandy-php.herokuapp.com/math.php",    
+            type: "post",    
             dataType: 'json',
-            data: {op: op,x: xval, y: yval},
+            data: {op: op, x: xval, y: yval},
             success:function(result){
                 showResult(result);
             },
